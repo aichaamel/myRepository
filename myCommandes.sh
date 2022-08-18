c18ade438cae50887462874cc9e51020421ca2c0
djelloul amel aicha <aichaamal.djelloul@yahoo.com>
2022-08-18 00:26:31 -0700

    l ajout de conig git

--- myCommandes.sh
+++ myCommandes.sh
@@ -121,8 +121,7 @@ git config --global alias.co (checkout)
 git config --global alias.br (branch)
 git config --global alias.ci (commit)
 git config --global alias.st (status) 
-
-
+//configurer email et mdp sur git // git config credential.helper store
 
  
 
